ab5bb21d64a7f31a5e10c105e81f6f60932a04bd
Stefan Rinke <sker65@gmail.com>
2021-04-02 13:21:27 +0200

    - even more docu

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -12,6 +12,13 @@ do
     filename=$(basename "$filepath")
 	  echo "converting $filename"
 	  docker run --rm --network=host -v $SRC/:/datain -v "$DEST/$outdir":/dataout $IMAGE --output-type pdf "/datain/$filename" "/dataout/$filename"
+    #
+    # TODO use pdftotext like
+    # >> pdftotext -f 1 -l 1 -layout $filename - | grep -Eq "(Rechnung|Pollin)"
+    # to generate more self descriptive filenames or subdirectories based on pdf content
+    # extracted_date=`grep -m 1 -o -E "(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})"`
+    #
+    # create a backup of the original file
 	  mkdir -p "$SRC/original/$outdir"
 	  mv "$SRC/$filename" "$SRC/original/$outdir/$filename"
 	  # by setting the exitcode to 1 if something was processed, you can get a mail from synology scheduler
