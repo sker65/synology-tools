e62a7d3fb360db2d1cc0496216c2b11bcfd18450
Stefan Rinke <sker65@gmail.com>
2021-04-02 13:06:21 +0200

    - improved doc

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ collection of scripts
 
 1. scan a simple cron scheduled script that applies OCR to scanned PDFs using the famous docker based ocrmypdf
 
+To use the ocrmypdf you need to install docker first, for aarch64 based synology device see
+https://stackoverflow.com/questions/52520008/can-i-install-docker-on-arm8-based-synology-nas
+
--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+# create a per date output directory
 outdir=`date +%Y_%m_%d`
 exitcode=0
-for filepath in /volume2/Share2/Scan/*.pdf
+SRC=/volume2/Share2/Scan
+DEST=/volume2/Share2/Documents
+# docker image to use
+IMAGE=andy008/ocrmypdf
+for filepath in $SRC/*.pdf
 do
-    if [ -e "$filepath" ]; then
-      filename=$(basename "$filepath")
+  if [ -e "$filepath" ]; then
+    filename=$(basename "$filepath")
 	  echo "converting $filename"
-	  docker run --rm --network=host -v /volume2/Share2/Scan/:/datain -v "/volume2/Share2/Documents/$outdir":/dataout andy008/ocrmypdf --output-type pdf "/datain/$filename" "/dataout/$filename"
-	  mkdir -p "/volume2/Share2/Scan/original/$outdir"
-	  mv "/volume2/Share2/Scan/$filename" "/volume2/Share2/Scan/original/$outdir/$filename"
-	  exitcode=1
+	  docker run --rm --network=host -v $SRC/:/datain -v "$DEST/$outdir":/dataout $IMAGE --output-type pdf "/datain/$filename" "/dataout/$filename"
+	  mkdir -p "$SRC/original/$outdir"
+	  mv "$SRC/$filename" "$SRC/original/$outdir/$filename"
+	  # by setting the exitcode to 1 if something was processed, you can get a mail from synology scheduler
+    exitcode=1
 	fi
 done
-chown -R sr.users "/volume2/Share2/Documents/$outdir"
+chown -R sr.users "$DEST/$outdir"
 # if we did not create anything remove the directory to avoid empty dirs
-rmdir --ignore-fail-on-non-empty "/volume2/Share2/Documents/$outdir"
+rmdir --ignore-fail-on-non-empty "$DEST/$outdir"
 exit $exitcode
